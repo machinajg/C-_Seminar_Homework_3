9336898e53badf384e78119231fe59fbcfd413d6
Julia Machina <machinajg@gmail.com>
2023-02-07 00:36:20 +0300

    5digit number palindrome project added

---
+++ 5digits_number_palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число:  ");
+int numA = Convert.ToInt32(Console.ReadLine());
+if ((numA/10000) == numA % 10)
+{
+    if ((numA/1000) % 10 == (numA/10) % 10)
+    Console.WriteLine($"{numA} - Палиндром!");
+    else
+    Console.WriteLine($"{numA} - НЕ Палиндром");
+}
+else
+    Console.WriteLine($"{numA} - НЕ Палиндром");
